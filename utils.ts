5e511d1534e7144d4483a5b905e453305b20d0b9
mynkbis <77193059+mynkbis@users.noreply.github.com>
2022-09-27 18:51:06 +0530

    Create utils.ts

---
+++ utils.ts
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+exports.populateUser = exports.showReviewTotal = void 0;
+var reviewTotalDisplay = document.querySelector('#reviews');
+var returningUserDisplay = document.querySelector('#returning-user');
+var userNameDisplay = document.querySelector('#user');
+function showReviewTotal(value, reviewer, isLoyalty) {
+    var iconDisplay = isLoyalty ? '⭐' : '';
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay;
+}
+exports.showReviewTotal = showReviewTotal;
+function populateUser(isReturning, userName) {
+    if (isReturning == true) {
+        returningUserDisplay.innerHTML = 'back';
+    }
+    userNameDisplay.innerHTML = userName;
+}
+exports.populateUser = populateUser;
